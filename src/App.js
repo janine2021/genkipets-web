e21f64a2d406b7eb129ecd82319e8987116fc101
Janine Laping <janine.sablesoftwares@gmail.com>
2024-04-08 03:35:55 +0800

    add Dashboard, Header, logo and modify landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="app">
-      <LandingPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          {/* Add other routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,24 @@
 import React, { useEffect } from 'react';
 import './LandingPage.css';
-import { setTitle } from '../title'; 
+import { setTitle } from '../title';
+import Dashboard from "./Dashboard";
 
 const LandingPage = () => {
 
   useEffect(() => {
-    setTitle("GenkiPets - Landing");
-  }, []); 
+    setTitle("GenkiPets - Home");
+  }, []);
 
   return (
     <div className="landing-page">
       <div className="welcome-note">
         <h1>Hi, welcome to GenkiPets!</h1>
         <p>Your assistant for taking good care of your furbabies.</p>
+      </div>
+      <div className="landing-body">
+        <div className="dashboard-section">
+          <Dashboard />
+        </div>
       </div>
     </div>
   );
